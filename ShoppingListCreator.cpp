3cbad68c904b51fb745c9721e19fb4e0a498aff0
Vaughn Potts <VWP92@users.noreply.github.com>
2017-10-08 00:36:34 -0400

    FirstCommit
    First commit of Cpp file written in VS2017.

---
+++ ShoppingListCreator.cpp
@@ -0,0 +1,145 @@
+// ShoppingListCreator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class itemType { 
+public:
+	struct aisle { //Items will be seperated by aisle using this struct
+		int typeNum;
+		string typeName;
+	};
+
+	aisle aisle;
+
+	vector<itemType> itemAisle;
+
+	void createAisle(vector<itemType> &x) { //Creates vector that contains Aisle options
+		vector<string> aisleNames = { "Fruits and Veggies", "Deli", "Bakery", "Seafood", "Dry Goods", "Dairy", "Frozen" };
+		itemType y;
+		for (int i = 0; i < aisleNames.size(); i++)
+		{
+			y.aisle.typeNum = i + 1;
+			y.aisle.typeName = aisleNames[i];
+			itemAisle.push_back(y);
+		}
+	}
+} type;
+
+class groceries {
+
+	struct itemStruct { //Parameters items contain
+		string name;
+		string quantity;
+		itemType aisleNumber;
+	};
+
+	itemStruct item;
+
+public:
+	vector<groceries> list; //Vector Groceries stored in
+
+	void printList(vector<groceries> &x, vector<itemType> &y) { //Print list of items and quantity
+		/*for (int i = 0; i < x.size(); i++) {
+			cout << x[i].item.quantity << " " << x[i].item.name << " (" << x[i].item.aisleNumber.aisle.typeName << ")" << endl;
+		}*/
+		//cout << "List exported to file." << endl << endl;
+
+		cout << "(Item's sorted by Aisle)" << endl;
+		for (int i = 0; i < y.size(); i++){ //sorts items by aisle in store
+			for (int o = 0; o < x.size(); o++) {
+				if (y[i].aisle.typeNum == x[o].item.aisleNumber.aisle.typeNum){
+					cout << x[o].item.quantity << " " << x[o].item.name << " (" << x[o].item.aisleNumber.aisle.typeName << ")" << endl;
+				}
+			}
+		}
+	}
+
+	void addItem(vector<groceries> &x, vector<itemType> &y) { //add item to list
+		groceries z;
+		string name;
+		string quantity;
+		itemType type; //User's selection in itemType data type
+		int typeNum; //Take number from user's selection
+
+		cout << "Enter name for new entry: ";
+		//cin >> name;
+		getline(cin, name);
+		cout << "How many?(Text Acceped): ";
+		//cin >> quantity;
+		getline(cin, quantity);
+		cout << endl;
+		for (int i = 0; i < y.size(); i++) { //Prints item type choices
+			cout << y[i].aisle.typeNum << " " << y[i].aisle.typeName << endl;
+		}
+		cout << "Please select number of aisle item belongs to: ";
+		cin >> typeNum;
+		type = y[typeNum - 1];
+
+		z.item.name = name;
+		z.item.quantity = quantity;
+		z.item.aisleNumber = type;
+		x.push_back(z);
+	}
+
+	void deleteItem(vector<groceries> &x) { //Remove item from list
+		int y;
+		for (int i = 0; i < x.size(); i++) {
+			cout << (i + 1) << ": " << x[i].item.quantity << " " << x[i].item.name << endl;
+		}
+		cout << "Type item number you would like to delete: ";
+		cin >> y;
+		x.erase(x.begin() + (y-1));
+	}
+} groc;
+
+void mainMenu() { //Menu that shows on program start up
+	cout << "-------------------------" << endl;
+	cout << "ShoppingListCreator!" << endl << endl;
+	cout << "Options: " << endl;
+	cout << "1 adds items" << endl;
+	cout << "2 deletes items" << endl;
+	cout << "3 prints items" << endl;
+	cout << "-------------------------" << endl;
+	cout << "Enter number for option you wish to select:  ";
+}
+
+int main() {
+	char again = 'y';
+	int choice;
+	type.createAisle(type.itemAisle);
+
+	while (again == 'y') {
+		mainMenu();
+		cin >> choice;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		while (choice != 1 && choice != 2 && choice != 3) { //Trap choices outside of options
+			cout << "Please choose options 1 to 3: ";
+			cin >> choice;
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		if (choice == 1) {
+			groc.addItem(groc.list, type.itemAisle);
+		}
+		else if (choice == 2) {
+			groc.deleteItem(groc.list);
+		}
+		else if (choice == 3) {
+			groc.printList(groc.list,type.itemAisle);
+		}
+		cout << "-------------------------" << endl;
+		cout << "Would you like to continue?(y/n): ";
+		cin >> again;
+		while (again != 'y' && again != 'Y' && again != 'n' && again != 'N') { //Traps choices outside of options
+			cout << "Please enter y for Yes or n for No: ";
+			cin >> again;
+		}
+	}
+	return 0;
+}
+
